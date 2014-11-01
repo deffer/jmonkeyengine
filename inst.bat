b6c14906b70eeb15a29bff4d66e4aba9e99a5934
deffer <irina.benediktovich@gmail.com>
2014-11-02 12:23:32 +1300

    Bat for installing some modules

---
+++ inst.bat
@@ -0,0 +1,13 @@
+cd jme3-core
+call mvn install
+cd ../jme3-desktop
+call mvn install
+cd ../jme3-effects
+call mvn install
+cd ../jme3-plugins
+call mvn install
+cd ../jme3-lwjgl
+call mvn install
+cd ../jme3-blender
+call mvn install
+cd ..
